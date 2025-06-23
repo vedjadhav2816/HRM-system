0dd7df9ac69ca53f8a055a86956ed0908f3494e6
Ved Jadhav <vedjadhav20@gmail.com>
2025-06-23 11:17:30 +0530

    🔧 Updated Axios URLs to use Railway backend

---
+++ src/axios.js
@@ -0,0 +1,6 @@
+// src/axios.js
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://hrm-system-production.up.railway.app',
+});
--- src/components/EditDepartment.js
+++ src/components/EditDepartment.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from './axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function EditDepartment() {
